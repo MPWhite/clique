6342f16c9d9c2211d0450e6a01a61e496c5f0da5
Matt White <mattp.white95@gmail.com>
2022-08-17 19:42:26 -0600

    Test trace

--- server/tracer.ts
+++ server/tracer.ts
@@ -1,12 +1,8 @@
 import tracer from "dd-trace";
 tracer.init(); // initialized in a different file to avoid hoisting.
 
-tracer.use("express", {
-  service: "clique-be",
-});
+tracer.use("express");
 
-tracer.use("pg", {
-  service: "pg-cluster",
-});
+tracer.use("http");
 
 export default tracer;
