cc982da79faa7f14d10f67acfc60300f27133e4e
Matt White <mattp.white95@gmail.com>
2022-08-17 19:49:25 -0600

    Test trace

--- server/tracer.ts
+++ server/tracer.ts
@@ -1,7 +1,24 @@
 import tracer from "dd-trace";
 tracer.init(); // initialized in a different file to avoid hoisting.
 
+const httpOptions = {
+  service: "test",
+  allowlist: ["url", /url/, (url) => true],
+  blocklist: ["url", /url/, (url) => true],
+  validateStatus: (code) => code < 400,
+  headers: ["host"],
+  middleware: true,
+};
+
+const httpServerOptions = {
+  ...httpOptions,
+  hooks: {
+    request: (span, req, res) => {},
+  },
+};
+
 tracer.use("express");
+tracer.use("express", httpServerOptions);
 tracer.use("http");
 tracer.use("http2");
 
