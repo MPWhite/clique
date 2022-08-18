31a628257f9a75ec9b1b73458273d967f1cb7d42
Matt White <mattp.white95@gmail.com>
2022-08-17 19:47:24 -0600

    Test trace

--- server/tracer.ts
+++ server/tracer.ts
@@ -2,7 +2,7 @@ import tracer from "dd-trace";
 tracer.init(); // initialized in a different file to avoid hoisting.
 
 tracer.use("express");
-
 tracer.use("http");
+tracer.use("http2");
 
 export default tracer;
