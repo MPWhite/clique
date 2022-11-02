b52657c4bd2620a537bfef72556fecc05346eda9
Matt <mattp.white95@gmail.com>
2022-11-01 22:40:14 -0400

    Remove redux-y stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.scss";
 import { PostForm } from "./features/postForm/PostForm";
 import { PostFeed } from "./features/postFeed/PostFeed";
